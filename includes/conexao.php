c51049a1764c5ddb2c3897dfbe39bea2056fa85d
traffego <traffego.mkt@gmail.com>
2026-02-24 15:57:29 -0300

    conexão php - data resolvida

--- includes/conexao.php
+++ includes/conexao.php
@@ -1,4 +1,7 @@
 <?php
+// Timezone DEVE ser definido antes de qualquer operação com datas
+date_default_timezone_set('America/Sao_Paulo');
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
